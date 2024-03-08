a2c9bb5f9e5a86676b16058badd48098ba7452be
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2024-03-08 14:33:28 +0530

    Create store.js

---
+++ src/store.js
@@ -0,0 +1,9 @@
+const redux = require('redux');
+
+const createStore = redux.createStore;
+
+const reducer =() =>{
+  // reducer function content - for more visit reducer.js file
+}
+
+const store = createStore(reducer);
